da0ce281c7c585ff07c6d92603952db96a9e1b37
silent_mantra <silentmantra.ru@gmail.com>
2026-06-05 01:56:55 +0200

    change fav icon

--- stereo-reader/app/assets/ItemItems-BvG1OsAY.js
+++ stereo-reader/app/assets/ItemItems-BvG1OsAY.js
@@ -1,4 +1,4 @@
-import{I as i,f as o,d as l,C as u,s as h}from"./App-2kO05AqG.js";import{t as c,h as f}from"./index-DDDmItjO.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
+import{I as i,f as o,d as l,C as u,s as h}from"./App-BMzQJGQV.js";import{t as c,h as f}from"./index-D0Jovw6w.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
