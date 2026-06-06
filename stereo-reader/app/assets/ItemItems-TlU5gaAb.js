4c001e1a8549742017a0bc8327edcc7f2489038b
silent_mantra <silentmantra.ru@gmail.com>
2026-06-06 03:24:58 +0200

    fix stereo reader page title for russian

--- stereo-reader/app/assets/ItemItems-TlU5gaAb.js
+++ stereo-reader/app/assets/ItemItems-TlU5gaAb.js
@@ -1,4 +1,4 @@
-import{I as i,f as o,d as l,C as u,s as h}from"./App-BoWiYdsO.js";import{t as c,h as f}from"./index-DrqdvLuB.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
+import{I as i,f as o,d as l,C as u,s as h}from"./App-Csm3q5K1.js";import{t as c,h as f}from"./index-81JnNrOt.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
