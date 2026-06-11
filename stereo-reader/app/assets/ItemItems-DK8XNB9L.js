52af3b46005ed7868f507600a27fd53b7587d1d4
silent_mantra <silentmantra.ru@gmail.com>
2026-06-11 18:26:16 +0200

    add stereo reader app beta copy

--- stereo-reader/app/assets/ItemItems-DK8XNB9L.js
+++ stereo-reader/app/assets/ItemItems-DK8XNB9L.js
@@ -1,4 +1,4 @@
-import{I as i,f as o,d as l,C as u,s as h}from"./App-D6sxUB1d.js";import{t as c,h as f}from"./index-DKTcuKG0.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
+import{I as i,f as o,d as l,C as u,s as h}from"./App-8V0rET6Q.js";import{t as c,h as f}from"./index-BoX9dIRq.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
