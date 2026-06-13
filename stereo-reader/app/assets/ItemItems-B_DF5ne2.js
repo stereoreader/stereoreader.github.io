ce68b4a765afcc283a9c16efc145570cdb1c92a5
silent_mantra <silentmantra.ru@gmail.com>
2026-06-13 21:08:48 +0200

    add app redirect

--- stereo-reader/app/assets/ItemItems-B_DF5ne2.js
+++ stereo-reader/app/assets/ItemItems-B_DF5ne2.js
@@ -1,4 +1,4 @@
-import{I as i,f as o,d as l,C as u,s as h}from"./App-DTj-wQyX.js";import{t as c,h as f}from"./index-5DZQG8B2.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
+import{I as i,f as o,d as l,C as u,s as h}from"./App-DOrECMeU.js";import{t as c,h as f}from"./index-DBkIKut4.js";class m extends u{async onClick(t){}childVNodes=(t,s)=>this.$item.thumbnailUrl?[f("div",{innerHTML:"&nbsp;",style:`
                 background:url(${this.$item.thumbnailUrl}) no-repeat top right;
                 background-size: auto 100%;
                 position:absolute;
