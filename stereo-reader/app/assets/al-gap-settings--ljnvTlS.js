3d0cd4722a06ad9059c3a321af99c21d2ad2eef0
silent_mantra <silentmantra.ru@gmail.com>
2026-07-02 21:17:07 +0200

    add push fx to cards

---
+++ stereo-reader/app/assets/al-gap-settings--ljnvTlS.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./App-Bs3dr2FE.js";import"./index-CB4XbK2n.js";export{m as default};
--- stereo-reader/app/assets/al-gap-settings-Gh73ghIP.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-Sz7_RXIo.js";import"./index-Sw5UIO3O.js";export{m as default};
