59229624d059c009cc0f26b1314148babe3558df
silent_mantra <silentmantra.ru@gmail.com>
2026-07-21 16:34:40 +0200

    work on al-log animation

--- stereo-reader/app/assets/al-gap-settings-By_WyEeZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-y4UixU2Z.js";import"./index-Rvfe2lT_.js";export{m as default};
---
+++ stereo-reader/app/assets/al-gap-settings-Bzs9J9kO.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./App-Y1YH5iwi.js";import"./index-DqZa2zGs.js";export{m as default};
