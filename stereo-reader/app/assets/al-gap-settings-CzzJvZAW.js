bc2f23541b256241db2dfaa2053d3379f18080a6
silent_mantra <silentmantra.ru@gmail.com>
2026-07-31 12:42:23 +0200

    add pointer angle tracking to card

--- stereo-reader/app/assets/al-gap-settings-CzzJvZAW.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-RccOavVY.js";import"./index-0u7CaPUv.js";export{m as default};
---
+++ stereo-reader/app/assets/al-width-settings-Dl4WeLpZ.js
@@ -0,0 +1,1 @@
+import{h as m}from"./App-Bbg7zksW.js";import"./index-B2RDdJe9.js";export{m as default};
