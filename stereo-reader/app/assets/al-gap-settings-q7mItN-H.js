3572d5b5c2acc173264016a9a925b4c2e5b64d88
silent_mantra <silentmantra.ru@gmail.com>
2026-08-23 23:47:22 +0200

    hide yt-chat since not working

---
+++ stereo-reader/app/assets/al-gap-settings-q7mItN-H.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./App-DzCJIbDv.js";import"./index-DJuaM3xB.js";export{m as default};
---
+++ stereo-reader/app/assets/al-width-settings-1MnUVv_w.js
@@ -0,0 +1,1 @@
+import{q as m}from"./App-DzCJIbDv.js";import"./index-DJuaM3xB.js";export{m as default};
