9c910e6b998784e16b5bd8f2a834ccabd9478e53
silent_mantra <silentmantra.ru@gmail.com>
2026-08-23 16:35:24 +0200

    rework stereo reader page

--- stereo-reader/app/assets/EditDialog-DAi5yfhk.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./EditDialog.vue_vue_type_script_setup_true_lang-DSvuw2z-.js";import"./EditDialog-CqfYPYL3.js";import"./App-D45VLyV2.js";import"./index-7EujlJrf.js";export{o as default};
--- stereo-reader/app/assets/al-gap-settings-LV6ok0jQ.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-D45VLyV2.js";import"./index-7EujlJrf.js";export{m as default};
