8325b764cfba821bba724a9712e4bd2541eda781
silent_mantra <silentmantra.ru@gmail.com>
2026-08-24 18:16:00 +0200

    fix parallel view hook

--- stereo-reader/app/assets/EditDialog-DFcylguW.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./EditDialog.vue_vue_type_script_setup_true_lang-CdknkNjK.js";import"./EditDialog-Bp6xG8My.js";import"./App-CJIG1Zfu.js";import"./index-CtT0z2t0.js";export{o as default};
--- stereo-reader/app/assets/al-gap-settings-xdciLwOH.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-CJIG1Zfu.js";import"./index-CtT0z2t0.js";export{m as default};
