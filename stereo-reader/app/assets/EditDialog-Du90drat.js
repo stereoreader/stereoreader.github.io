a1631d4412d401d9ed4254edd80ee4ee01c59ea9
silent_mantra <silentmantra.ru@gmail.com>
2026-08-30 19:06:13 +0200

    use useAsyncData with article and experience pages

--- stereo-reader/app/assets/EditDialog-Du90drat.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./EditDialog.vue_vue_type_script_setup_true_lang-CgVGXIXd.js";import"./EditDialog-CVlKGSzv.js";import"./App-BZTqEZGx.js";import"./index-DtcoFVSQ.js";export{o as default};
--- stereo-reader/app/assets/al-width-settings-BBIfHckR.js
+++
@@ -1,1 +0,0 @@
-import{v as m}from"./App-BZTqEZGx.js";import"./index-DtcoFVSQ.js";export{m as default};
