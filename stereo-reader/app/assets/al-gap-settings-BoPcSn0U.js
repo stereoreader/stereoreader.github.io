b8f39604b673b6b69754d1b4d419fdc8bbd4bea2
silent_mantra <silentmantra.ru@gmail.com>
2026-08-31 22:06:34 +0200

    add stereo reader training position

--- stereo-reader/app/assets/al-gap-settings-BoPcSn0U.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-AHKFlhaA.js";import"./index-BDsN8B9a.js";export{m as default};
--- stereo-reader/app/assets/al-width-settings-B2UO0lqh.js
+++
@@ -1,1 +0,0 @@
-import{v as m}from"./App-AHKFlhaA.js";import"./index-BDsN8B9a.js";export{m as default};
