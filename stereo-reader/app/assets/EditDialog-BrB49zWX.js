57cbcd607f053acdcf90a30d67e2aad7a590f23e
silent_mantra <silentmantra.ru@gmail.com>
2026-09-06 17:24:22 +0200

    work on links

--- stereo-reader/app/assets/EditDialog-BrB49zWX.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./EditDialog.vue_vue_type_script_setup_true_lang-CWdOoXSl.js";import"./EditDialog-lwTItj6w.js";import"./App-DUCD6oLI.js";import"./index-D-Wx4md4.js";export{o as default};
--- stereo-reader/app/assets/al-width-settings-B2XCqFbc.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./App-DUCD6oLI.js";import"./index-D-Wx4md4.js";export{m as default};
