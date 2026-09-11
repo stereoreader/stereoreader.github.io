080cd8c1690dec8e09be2977730f46b636f6c8e7
silent_mantra <silentmantra.ru@gmail.com>
2026-09-11 14:48:12 +0200

    add answer count to links

--- stereo-reader/app/assets/al-gap-settings-CjHjx2TV.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-BhHtVPvy.js";import"./index-DwyPKag7.js";export{m as default};
--- stereo-reader/app/assets/al-width-settings-DqDeX0cf.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./App-BhHtVPvy.js";import"./index-DwyPKag7.js";export{m as default};
