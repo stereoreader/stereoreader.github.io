e096d8424151b46b38313907ea76b602b41ad12d
silent_mantra <silentmantra.ru@gmail.com>
2026-09-15 16:12:04 +0200

    add hebrew for stereo reader

--- stereo-reader/app-beta/assets/EditDialog-DQFH-Tjl.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./EditDialog.vue_vue_type_script_setup_true_lang-Dh-vn3ph.js";import"./EditDialog-CK2D301U.js";import"./App-Dp8SQKcu.js";import"./index-CGW8Q1Jh.js";export{o as default};
--- stereo-reader/app-beta/assets/al-gap-settings-BIanM09j.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./App-Dp8SQKcu.js";import"./index-CGW8Q1Jh.js";export{m as default};
