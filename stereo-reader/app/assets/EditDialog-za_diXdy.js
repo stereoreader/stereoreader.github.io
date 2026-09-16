fe504490758d9b613f157cc16b0bd1fdce466ad3
silent_mantra <silentmantra.ru@gmail.com>
2026-09-16 15:23:40 +0200

    move stereo reader to stereo.aleklabs.dev

--- stereo-reader/app/assets/EditDialog-za_diXdy.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./EditDialog.vue_vue_type_script_setup_true_lang-BOjbFj8E.js";import"./EditDialog-C7qG2YeT.js";import"./App-iaYzInfc.js";import"./index-DrF8TeVB.js";export{o as default};
--- stereo-reader/app/assets/al-width-settings-CEQW4l2-.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./App-iaYzInfc.js";import"./index-DrF8TeVB.js";export{m as default};
