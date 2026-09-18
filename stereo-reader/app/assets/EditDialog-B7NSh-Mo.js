59a5685b0777e96dfd3b507852772cf1640e05d8
silent_mantra <silentmantra.ru@gmail.com>
2026-09-18 02:44:41 +0200

    update stereo reader demo training

---
+++ stereo-reader/app/assets/EditDialog-B7NSh-Mo.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./EditDialog.vue_vue_type_script_setup_true_lang-xxdFMJxF.js";import"./EditDialog-DXhl1ZBL.js";import"./App-DSQ-VU8a.js";import"./index-Bx6Wd-h3.js";export{o as default};
---
+++ stereo-reader/app/assets/al-width-settings-DF5KJwbM.js
@@ -0,0 +1,1 @@
+import{j as m}from"./App-DSQ-VU8a.js";import"./index-Bx6Wd-h3.js";export{m as default};
