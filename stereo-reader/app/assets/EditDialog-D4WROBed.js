f36300ac3ff7d3c592f5ed3656fac6db940c4ab4
silent_mantra <silentmantra.ru@gmail.com>
2026-09-24 22:22:36 +0200

    return back the chrome youtube extension card

---
+++ stereo-reader/app/assets/EditDialog-D4WROBed.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./EditDialog.vue_vue_type_script_setup_true_lang-CUf1Pw5w.js";import"./EditDialog-E9Y80KsI.js";import"./App-BcbwdnId.js";import"./index-CstQ5SBo.js";export{o as default};
---
+++ stereo-reader/app/assets/al-gap-settings-v2x3Lwfp.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./App-BcbwdnId.js";import"./index-CstQ5SBo.js";export{m as default};
